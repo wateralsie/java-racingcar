d8fc370c5ef304917dca2fc6f03f9de65ea76fc6
wateralsie <minee1913@gmail.com>
2025-10-22 16:16:27 +0900

    feat: 쉼표를 기준으로 자동차 이름 분리 후 자동차 객체 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,39 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
+        Application app = new Application();
+
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
+        Set<Car> cars = app.addCarsToRace(app.parse(carNames));
         System.out.println("시도할 횟수는 몇 회인가요?");
+        int tries;
         try {
-            int tries = Integer.parseInt(Console.readLine());
+            tries = Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도할 횟수는 숫자 형태로 입력해주세요.");
         }
+        Set<Car> cars = app.addCarsToRace(app.parse(carNames));
+    }
+
+    public String[] parse(String names) {
+        String[] namesList = names.split(",");
+        if (namesList.length < 2) {
+            throw new IllegalArgumentException("자동차 이름은 두 개 이상 쉼표로 구분하여 입력해주세요.");
+        }
+        return namesList;
+    }
+
+    public Set<Car> addCarsToRace(String[] names) {
+        Set<Car> cars = new LinkedHashSet<>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+        return cars;
     }
 }
