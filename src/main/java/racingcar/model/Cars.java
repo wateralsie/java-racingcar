3727b288dbf27f7498ef07851052bee73a7e803e
wateralsie <minee1913@gmail.com>
2025-10-27 01:49:29 +0900

    refactor: 자동차 일급 컬렉션에 정적 팩토리 메서드 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,49 +1,48 @@
 package racingcar.model;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import racingcar.constants.ErrorMessage;
 
 public class Cars {
-    private List<Car> cars;
+    private final List<Car> cars;
 
     private Cars(List<Car> cars) {
-        this.cars = cars;
+        this.cars = new ArrayList<>(cars);
     }
 
-    public Cars addCarsToRace(String namesStr) {
-        List<String> names = getNamesFrom(namesStr);
-        for (String name : names) {
-            cars.add(new Car(name));
-        }
-        return new Cars(cars);
+    public static Cars from(List<String> carNames) {
+        validate(carNames);
+        List<Car> carList = carNames.stream()
+                .map(Car::new)
+                .toList();
+        return new Cars(carList);
     }
 
-    private List<String> getNamesFrom(String namesStr) {
-        String[] names = namesStr.split(",");
-        validate(names);
-        return Arrays.asList(names);
+    private static void validate(List<String> names) {
+        validateMinimumCars(names);
+        validateDuplicateNames(names);
     }
 
-    private void validate(String[] names) {
-        checkMinimumCars(names);
-        checkDuplicateNames(names);
-    }
-
-    private void checkMinimumCars(String[] names) {
-        if (names.length < 2) {
+    private static void validateMinimumCars(List<String> names) {
+        if (names.size() < 2) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAMES);
         }
     }
 
-    private void checkDuplicateNames(String[] names) {
+    private static void validateDuplicateNames(List<String> names) {
         Set<String> uniqueNames = new HashSet<>();
         for (String name : names) {
             if (!uniqueNames.add(name)) {
                 throw new IllegalArgumentException(ErrorMessage.DUPLICATE_CAR_NAMES);
             }
         }
+    }
+
+    public List<Car> getAll() {
+        return Collections.unmodifiableList(cars);
     }
 }
