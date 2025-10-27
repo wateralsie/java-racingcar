fdea85fd15fd3baaeb9e01908f5c5e3751258f69
wateralsie <minee1913@gmail.com>
2025-10-27 10:20:22 +0900

    refactor: controller를 통해 자동차 경주 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,75 +1,9 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-import racingcar.constants.CarRaceMessage;
-import racingcar.constants.ErrorMessage;
+import racingcar.controller.CarRaceController;
 
 public class Application {
     public static void main(String[] args) {
-        Application app = new Application();
-
-        System.out.println(CarRaceMessage.CAR_NAMES_ENTER_PROMPT);
-        String carNames = Console.readLine();
-        Set<Car> cars = app.addCarsToRace(app.parse(carNames));
-        System.out.println(CarRaceMessage.TRY_NUMBER_ENTER_PROMPT);
-        int tries;
-        try {
-            tries = Integer.parseInt(Console.readLine());
-            if (tries <= 0) {
-                throw new IllegalArgumentException(ErrorMessage.TRY_NUMBER_NOT_POSITIVE);
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.TRY_NUMBER_NOT_NUMBER);
-        }
-        System.out.println("\n실행 결과");
-        for (int i = 0; i < tries; i++) {
-            for (Car car : cars) {
-                System.out.print(car.getName() + " : ");
-                int num = Randoms.pickNumberInRange(0, 9);
-                if (num >= 4) {
-                    car.goForward();
-                }
-                System.out.println("-".repeat(car.getDistance()));
-            }
-            System.out.println();
-        }
-        System.out.println("최종 우승자 : " + String.join(", ", app.getWinnerNames(cars)));
-    }
-
-    public List<String> getWinnerNames(Set<Car> cars) {
-        int maxDistance = cars.stream().mapToInt(Car::getDistance).max().orElse(0);
-        return cars.stream()
-                .filter(car -> car.getDistance() == maxDistance)
-                .map(Car::getName)
-                .toList();
-    }
-
-    public Set<String> parse(String namesStr) {
-        String[] names = namesStr.split(",");
-        if (names.length < 2) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAMES);
-        }
-        Set<String> uniqueNames = new LinkedHashSet<>();
-        for (String name : names) {
-            if (name.length() > 6) {
-                throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH_OVER_FIVE);
-            }
-            if (!uniqueNames.add(name)) {
-                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_CAR_NAMES);
-            }
-        }
-        return uniqueNames;
-    }
-
-    public Set<Car> addCarsToRace(Set<String> names) {
-        Set<Car> cars = new LinkedHashSet<>();
-        for (String name : names) {
-            cars.add(new Car(name));
-        }
-        return cars;
+        CarRaceController.run();
     }
 }
---
+++ src/main/java/racingcar/controller/CarRaceController.java
@@ -0,0 +1,16 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.service.CarRaceService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class CarRaceController {
+    public static void run() {
+        List<String> carNames = InputView.readCarNames();
+        String triesStr = InputView.readTryNumber();
+        CarRaceService carRaceService = new CarRaceService(triesStr, carNames);
+        carRaceService.start();
+        OutputView.printWinners(carRaceService.getWinnerNames());
+    }
+}
