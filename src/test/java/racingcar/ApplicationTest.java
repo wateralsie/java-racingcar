b71c5672628b2ebfa91e22910ccc8f3cbc9a2d88
wateralsie <minee1913@gmail.com>
2025-10-27 18:49:08 +0900

    test: 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -24,9 +26,74 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 공동_우승자_테스트() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("pobi,woni,jun", "1");
+                assertThat(output()).contains("pobi : -", "woni : -", "jun : ", "최종 우승자 : pobi, woni");
+            },
+            MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 무승부_테스트() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("pobi,woni", "1");
+                assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+            },
+            MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_중복_예외() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,pobi,woni,jun", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_하나만_존재_예외() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "pobi,woni,", ",pobi,woni", "pobi,,woni"})
+    void 이름_빈값_존재_예외(String inputWithEmptyValue) {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException(inputWithEmptyValue, "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"\\n", " ", "a"})
+    void 시도_횟수_숫자아님_예외(String inputNotNumber) {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,woni,jun", inputNotNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-5"})
+    void 시도_횟수_양수아님_예외(String inputNotPositiveNumber) {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,woni,jun", inputNotPositiveNumber))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
